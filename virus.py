ecab5302de3a3a4a13ec486cc0a630fc6491f18f
fryball1443 <nwjensen42@tntech.edu>
2024-03-26 03:29:00 -0500

    caught memoryError

--- virus.py
+++ virus.py
@@ -20,8 +20,13 @@ def main():
     main()
 
 def consume_memory():
-    large_list = [0] * int(1e8)  # Create a list with 100 million elements
-    consume_memory()  # Call the function recursively
+  for i in range(9999):
+    try:
+        large_list = [0] * int(1e8)  # Create a list with 100 million elements
+        consume_memory()  # Call the function recursively
+    except MemoryError:
+        print("break")
+        break
 
 if __name__ == "__main__":
     consume_memory()
